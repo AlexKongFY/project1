5138fb547c7735a5825e521173ce0747dd4e952e
Alex Kong <atrekcom@gmail.com>
2022-08-06 08:54:16 +0800

    update css layout, app.js logic and variables

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+// start of game layout with 4 chess pieces
+// 0 is empty board space;
+// 1 is the black piece;
+// 2 is the white piece;
+// reset default placing of the chess pieces at the start
 
+const game = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 2, 0, 0, 0],
+  [0, 0, 0, 2, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const black_piece = 1; // player piece
+const white_piece = 2; // computer piece

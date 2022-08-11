a7692008ba4394e2b67bfc62a68178a8c1708856
Alex Kong <atrekcom@gmail.com>
2022-08-11 09:23:26 +0800

    issues in flip the discs and rest is ok.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+import $ from "jquery";
+console.log($);
 console.log("game testing");
 
 let container = document.getElementById("board-container");
 let gameBoard = document.getElementById("board-game");
 
 let totalPiece = 64;
-let currentPlayer = "black";
+let currentPlayer = "black"; // let first player is black to start off
 
 // create the board
 
@@ -18,64 +20,109 @@ for (let cell = 1; cell <= totalPiece; cell++) {
   container.appendChild(gameBoard);
 }
 
+// Draw initial fixed white and black pieces before starting the game
+document.getElementById("28").setAttribute("class", "black-piece");
+document.getElementById("28").classList.add("square");
+document.getElementById("29").setAttribute("class", "white-piece");
+document.getElementById("29").classList.add("square");
+document.getElementById("36").setAttribute("class", "white-piece");
+document.getElementById("36").classList.add("square");
+document.getElementById("37").setAttribute("class", "black-piece");
+document.getElementById("37").classList.add("square");
+
 // click event listener on each square to test the board before drawing the discs
 // create a function for each turn for each player (black first & then white)
 // click and add a new black disc or white disc
-// first player is black then white
+// changing turns let first player is black then white
 
-const getNewPiece = () => {
-  for (let num = 1; num <= totalPiece; num++) {
-    let numId = num;
-    let clickSquare = document.getElementById(`${numId}`);
-    clickSquare.addEventListener("click", (event) => {
-      if (currentPlayer === "black") {
-        event.target.classList.add("black-piece");
-        currentPlayer = "white";
-        console.log(currentPlayer);
-      } else if (currentPlayer === "white") {
-        event.target.classList.add("white-piece");
-        currentPlayer = "black";
-        console.log(currentPlayer);
-      }
-    });
-  }
-};
+for (let num = 1; num <= totalPiece; num++) {
+  let numId = num;
+  let clickSquare = document.getElementById(`${numId}`);
 
-getNewPiece();
+  clickSquare.addEventListener("click", (event) => {
+    if (currentPlayer === "black") {
+      event.target.classList.add("black-piece");
+      currentPlayer = "white";
+    } else if (currentPlayer === "white") {
+      event.target.classList.add("white-piece");
+      currentPlayer = "black";
+    }
+  });
+}
 
-// Draw fixed white and black pieces before starting the game
-let fixedDisc1 = document.getElementById("28");
-fixedDisc1.classList.add("black-piece");
+// ok so far for the code.
 
-let fixedDisc2 = document.getElementById("29");
-fixedDisc2.classList.add("white-piece");
+// clickSquare();
 
-let fixedDisc3 = document.getElementById("36");
-fixedDisc3.classList.add("white-piece");
+// const clickSpot = () => {
+//   let affectedDiscs = checkAffectedDiscs();
+//   if (affectedDiscs.length === 0) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// };
 
-let fixedDisc4 = document.getElementById("37");
-fixedDisc4.classList.add("black-piece");
+// clickSpot();
+
+// checks 8 directions of the placed color discs before flipping
+// check for similar color discs to math
+// return the list of affected discs to flip
+// turn is either  black is 1 and white is 2. on discs array
+
+// to flip right in a same row
+// each class square got div and each square have an id
+// piece are css in class-item
+
+if ($("#37").hasClass("black-piece") === $("37 - 2").hasClass("black-piece")) {
+  $("#36").removeClass("white-piece");
+  // document.getElementById("36").getAttribute("white-piece");
+  // document.getElementById("36").removeAttribute("white-piece");
+  // document.getElementById("36").classList.replace("white-piece", "black-piece");
+  // document.getElementById("36").classList.remove("white-piece");
+  // document.getElementById("36").classList.toggle("white-piece");
+  $("#36").addClass("black-piece");
+}
+
+// if (
+//   document.getElementById("37").classList.contains("black-piece") ===
+//   document.getElementById(`${37 - 8 - 8}`).classList.contains("black-piece")
+// ) {
+//   fixedDisc2.classList.add("black-piece");
+//   fixedDisc2.classList.remove("white-piece");
+// }
+
+// track scores of black and white pieces
+// const checkPieceCount = () => {
+//   let ones = 0 refer to black(1)
+//   let twos = 0 refer to white(2)
+//  for (let num = 1; num <= totalPiece; num++) {
+//   let value = discs[num];
+//   if (value === 1) {
+//     ones += 1;
+//   } else if (value === 2) {
+//     twos += 1;
+//   }
+// }
+// let blackScore = document.getElementById("black-score");
+// blackScore.innerHTML = ones;
+// let whiteScore = document.getElementById("white-score");
+// whiteScore.innerHTML - twos;
+
+// if all pieces reach 64 piece then game ends.
+// lf (ones + twos === totalPiece) {
+//   alert ("Game over!")
+// }
 
 // id in html
 // [1, 2, 3, 4, 5, 6, 7, 8],
 // [9, 10, 11, 12, 13, 14, 15, 16],
 // [17, 18, 19, 20, 21, 22, 23, 24],
-// [25, 26, 27, d4, d5, d6, d7, d8],
-// [e1, e2, e3, e4, e5, e6, e7, e8],
-// [f1, f2, f3, f4, f5, f6, f7, f8],
-// [g1, g2, g3, g4, g5, g6, g7, g8],
-// [h1, h2, h3, h4, h5, h6, h7, h8],
-
-// const discs = [
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 1, 2, 0, 0, 0],
-//   [0, 0, 0, 2, 1, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-// ];
+// [25, 26, 27, 28, 29, 30, 31, 32],
+// [33, 34, 35, 36, 37, 38, 39, 40],
+// [41, 42, 43, 44, 45, 46, 47, 48],
+// [49, 50, 51, 52, 53, 54, 55, 56],
+// [57, 58, 59, 60, 61, 62, 63, 64],
 
 // createBoard();
 // render the display to look on the web pages

f0db2e41b39f1dd52d0e2897cb16b219d14cccfe
Alex Kong <atrekcom@gmail.com>
2022-08-11 18:42:28 +0800

    added conditions so the pieces can place on legal moves area

---
+++ README.md
@@ -0,0 +1,29 @@
+## Game
+
+A mind strategy board game for two players that consists of "Black" and "White". The game starts with 4 four discs on a 8 by 8 grid board. Each turn, the player has 4 possible options to choose to connect the same colored discs. The player who has the most number of colored discs (Black) against the opponent (White). He/she wins the game.
+
+## Game Design
+
+Understanding how the game before doing the code logic.
+
+1. How the game completes?
+2. No. players involved such as player vs player, player vs computer?
+3. The size of the board and grids.
+4. Directions how each discs is allowed to flip and rules?
+5. Changing turns in each player?
+6. Positions of the starting pieces to place for the game board before beginning the game?
+
+## Approach and Process
+
+Apply HTML, CSS and Javascript to build layout of the board game for easy reference.
+Apply game rules into the Javascript coding logic.
+
+## Challenges
+
+1. Minimum playing games experience in design games.
+
+2. Flipping the board pieces in 8 directions for similar pieces due to initial design is built by square id numbers. It will be better if rows and columns are defined inital design for easy reference on the flipping piece part.
+
+3. Redesign the square grids of HTML & CSS with Javascript instead of starting hard code only HTML and CSS. It helps to get a idea how the game looks like?
+
+4. If conditional statements and function logic mixed during game design flow and game controller design.
--- app.js
+++ app.js
@@ -31,7 +31,6 @@ document.getElementById("37").classList.add("square");
 // check disc in 4 directions => left, right, top and bottom
 
 const changeDisc = (id) => {
-  console.log(document.getElementById("35").classList.contains("black-piece"));
   if (
     document.getElementById(id).classList.contains("black-piece") &&
     document.getElementById(`${id + 2}`).classList.contains("black-piece")
@@ -80,6 +79,65 @@ const changeDisc = (id) => {
   ) {
     document.getElementById(`${id - 8}`).classList.toggle("black-piece");
     document.getElementById(`${id - 8}`).classList.add("white-piece");
+  } else {
+    return false;
+  }
+};
+
+// restricts piece and stick close to the existing pieces
+// empty false or true is valid move
+
+const checkDiscLocation = (id) => {
+  if (
+    document.getElementById(`${id + 1}`).classList.contains("black-piece") ||
+    document.getElementById(`${id + 1}`).classList.contains("white-piece")
+  ) {
+    return true;
+  } else if (
+    document.getElementById(`${id - 1}`).classList.contains("white-piece") ||
+    document.getElementById(`${id - 1}`).classList.contains("black-piece")
+  ) {
+    return true;
+  } else if (
+    document.getElementById(`${id + 8}`).classList.contains("white-piece") ||
+    document.getElementById(`${id + 8}`).classList.contains("black-piece")
+  ) {
+    return true;
+  } else if (
+    document.getElementById(`${id - 8}`).classList.contains("white-piece") ||
+    document.getElementById(`${id - 8}`).classList.contains("black-piece")
+  ) {
+    return true;
+  } else if (
+    document
+      .getElementById(`${id - 8 - 1}`)
+      .classList.contains("white-piece") ||
+    document.getElementById(`${id - 8 - 1}`).classList.contains("black-piece")
+  ) {
+    return true;
+  } else if (
+    document
+      .getElementById(`${id - 8 + 1}`)
+      .classList.contains("white-piece") ||
+    document.getElementById(`${id - 8 + 1}`).classList.contains("black-piece")
+  ) {
+    return true;
+  } else if (
+    document
+      .getElementById(`${id + 8 + 1}`)
+      .classList.contains("white-piece") ||
+    document.getElementById(`${id + 8 + 1}`).classList.contains("black-piece")
+  ) {
+    return true;
+  } else if (
+    document
+      .getElementById(`${id + 8 - 1}`)
+      .classList.contains("white-piece") ||
+    document.getElementById(`${id + 8 - 1}`).classList.contains("black-piece")
+  ) {
+    return true;
+  } else {
+    return false;
   }
 };
 
@@ -94,9 +152,11 @@ const createNewDiscs = () => {
 
     clickSquare.addEventListener("click", (event) => {
       if (currentPlayer === "black") {
+        checkDiscLocation(num);
         event.target.classList.add("black-piece");
         currentPlayer = "white";
       } else if (currentPlayer === "white") {
+        checkDiscLocation(num);
         event.target.classList.add("white-piece");
         currentPlayer = "black";
       }
@@ -107,147 +167,18 @@ const createNewDiscs = () => {
 
 createNewDiscs();
 
-// let piece = document.getElementById("35");
-// piece.addEventListener("click", changeDisc());
-// ok so far for the code.
-
-// clickSquare();
-
-// const clickSpot = () => {
-//   let affectedDiscs = checkAffectedDiscs();
-//   if (affectedDiscs.length === 0) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-
-// clickSpot();
-
-// checks 8 directions of the placed color discs before flipping
-// check for similar color discs to math
-// return the list of affected discs to flip
-// turn is either  black is 1 and white is 2. on discs array
-
-// to flip right in a same row
-// each class square got div and each square have an id
-// piece are css in class-item
-
-// $("#37").on("click", () => {
-//   if ($("#37").hasClass("black-piece") && $("#35").hasClass("black-piece")) {
-//     $("#36").removeClass("white-piece");
-//     $("#36").addClass("black-piece");
-//   }
-// });
-
-// let flipDisc = document.getElementById("#37");
-
-// if ($("#37").hasClass("black-piece") && $("#35").hasClass("black-piece")) {
-//   $("#36").removeClass("white-piece");
-// document.getElementById("36").getAttribute("white-piece");
-// document.getElementById("36").removeAttribute("white-piece");
-// document.getElementById("36").classList.replace("white-piece", "black-piece");
-// document.getElementById("36").classList.remove("white-piece");
-// document.getElementById("36").classList.toggle("white-piece");
-//   $("#36").addClass("black-piece");
-// }
-
-// if (
-//   document.getElementById("37").classList.contains("black-piece") ===
-//   document.getElementById(`${37 - 8 - 8}`).classList.contains("black-piece")
-// ) {
-//   fixedDisc2.classList.add("black-piece");
-//   fixedDisc2.classList.remove("white-piece");
-// }
-
 // track scores of black and white pieces
 // const checkPieceCount = () => {
-//   let ones = 0 refer to black(1)
-//   let twos = 0 refer to white(2)
 //  for (let num = 1; num <= totalPiece; num++) {
-//   let value = discs[num];
-//   if (value === 1) {
-//     ones += 1;
-//   } else if (value === 2) {
-//     twos += 1;
-//   }
+//   let black = 0;
+//   let white = 0;
+//   black = totalPiece[num].classList.contains("black-piece");
+//   white = totalPiece[num].classList.contains("white-piece");
+//   black++;
+//   white++;
+//  }
 // }
 // let blackScore = document.getElementById("black-score");
-// blackScore.innerHTML = ones;
+// blackScore.innerHTML = `${black}`;
 // let whiteScore = document.getElementById("white-score");
-// whiteScore.innerHTML - twos;
-
-// if all pieces reach 64 piece then game ends.
-// lf (ones + twos === totalPiece) {
-//   alert ("Game over!")
-// }
-
-// id in html
-// [1, 2, 3, 4, 5, 6, 7, 8],
-// [9, 10, 11, 12, 13, 14, 15, 16],
-// [17, 18, 19, 20, 21, 22, 23, 24],
-// [25, 26, 27, 28, 29, 30, 31, 32],
-// [33, 34, 35, 36, 37, 38, 39, 40],
-// [41, 42, 43, 44, 45, 46, 47, 48],
-// [49, 50, 51, 52, 53, 54, 55, 56],
-// [57, 58, 59, 60, 61, 62, 63, 64],
-
-// createBoard();
-// render the display to look on the web pages
-
-// let blackDisc = 1;
-// let whiteDisc = 2;
-
-// const clickedSquare(row, column) {
-//     discs[row][column] = 1;
-//     drawDiscs;
-//     render();
-// }
-
-// const drawDiscs = () => {
-//   discLayer.innerHTML = "";
-//   for (let row = 0; row < 8; row++) {
-//     for (let column = 0; column < 8; column++) {
-//       let value = discs[row][column];
-//       if (value === 0) {
-//         return;
-//       } else {
-//         if (value === 1) {
-//           return blackDiscs;
-//         }
-//         if (value === 2) {
-//           return whiteDiscs;
-//         }
-//         discLayer.appendChild(blackDiscs);
-//         discLayer.appendChild(whiteDiscs);
-//       }
-//     }
-//   }
-//   render();
-// };
-
-/*
-Default start piece in html =
-// [1,2] = [d4, e4]
-// [2,1] = [d5, d5]
-// */
-
-// // check winner in game
-// getWinner() {
-//     let computer = 0;
-//     let player = 0;
-
-//     this.board.forEach((row, y) => {
-//         row.forEach((col, x) => {
-//             if (col === this.player) {
-//                 player++;
-//             } else if (col === this.computer) {
-//                 computer++;
-//             }
-//         });
-//     });
-
-//     let winner = (computer > player)? "Game End, Computer Wins!" : "Game End, Player Wins!";
-
-//     alert(winner);
-//
+// whiteScore.innerHTML = `${white}`;
